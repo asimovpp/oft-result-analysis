73805b2ad04cec330731386034c80c4a2d45ea4a
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2021-02-11 11:57:10 +0000

    Interim snapshot

--- analyse_scale.py
+++ analyse_scale.py
@@ -1,8 +1,11 @@
-import sys
+import sys, os
 import re
 import pandas as pd
 import numpy as np
 from plotnine import *
+import json
+from sklearn.linear_model import Ridge
+
 
 rx_dict = {"data_line": re.compile(r"Rank (-?\d+); Value (\d+) maxed at (-?\d+)")}
 
@@ -47,7 +50,22 @@ def almost_zero(group):
 
 
 def calc_trend(group):
-    slope = np.polyfit(group.world_size, group.value, deg=1)[0]
+    fitted = np.polyfit(group.world_size, group.value, deg=2)
+    print(fitted)
+    slope = fitted[0]
+    tolerance = 1e-10
+    if np.abs(slope) < tolerance:
+        slope = 0.0
+    return pd.Series(slope, index=["trend_slope"])
+
+
+def calc_ridge(group):
+    clf = Ridge(alpha=1.0)
+    clf.fit(X, y)
+
+    fitted = np.polyfit(group.world_size, group.value, deg=2)
+    print(fitted)
+    slope = fitted[0]
     tolerance = 1e-10
     if np.abs(slope) < tolerance:
         slope = 0.0
@@ -64,14 +82,30 @@ def classify_trend(row):
         return "negative"
 
 
+######################## main ########################
+
 all_data = []
-for filename in sys.argv[1:]:
-    all_data = all_data + parse_file(filename)
+#for filename in sys.argv[1:]:
+#    all_data = all_data + parse_file(filename)
+
+dirname=os.path.dirname(sys.argv[1])
+print(dirname)
+with open(sys.argv[1]) as f:
+    inputs = json.load(f)["inputs"]
+
+for ip in inputs:
+    scale = ip["scale"]
+    filename = ip["oft_output_filename"]
+    data = parse_file(dirname + "/" + filename)
+    for d in data:
+        d["scale"] = scale
+    all_data = all_data + data 
 
 df = pd.DataFrame(all_data)
 df2 = df[df.value != -1]
 #find max values in table grouped by world_size and value_id, i.e. max across ranks
-df3 = df2.groupby(["world_size", "value_id"]).max().reset_index()
+#df3 = df2.groupby(["world_size", "value_id"]).max().reset_index()
+df3 = df2.groupby(["scale", "value_id"]).max().reset_index()
 
 #(ggplot(df3) + aes(x="world_size", y="value", color="factor(value_id)") + geom_line() + geom_point())
 
@@ -87,7 +121,8 @@ df3 = df3.merge(slopes, on="value_id", how="left")
 
 df3["slope_classification"] = df3.apply(classify_trend, axis=1)
 
-(ggplot(df3) + aes(x="world_size", y="value", color="factor(value_id)") + geom_line() + geom_point() + scale_y_log10() + facet_wrap("slope_classification")).save("data_summary.pdf")
+#(ggplot(df3) + aes(x="world_size", y="value", color="factor(value_id)") + theme(legend_position='none') + geom_line() + geom_point() + scale_y_log10() + facet_wrap("slope_classification")).save("data_summary.pdf")
+(ggplot(df3) + aes(x="scale", y="value", color="factor(value_id)") + theme(legend_position='none') + geom_line() + geom_point() + facet_wrap("slope_classification") + geom_hline(yintercept = (2**31 - 1))).save("data_summary.pdf")
 
 df3.to_csv("data_summary.csv")
 
